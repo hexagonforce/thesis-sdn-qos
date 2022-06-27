3dcfc00f25811d7b734ef25f403611595df0db31
Hyeong Seon Yoo (유형선) <sylvesteryoo@naver.com>
2022-06-27 13:37:47 +0800

    fixed some utility stuff

--- setqos.sh
+++ setqos.sh
@@ -1,5 +1,2 @@
-#! /usr/bin/bash
-sudo ovs-vsctl --all destroy qos
-sudo ovs-vsctl --all destroy queue
 chmod u+x ./scripts/custom/run.ovs-vsctl.case.$1.sh
 ./scripts/custom/run.ovs-vsctl.case.$1.sh
