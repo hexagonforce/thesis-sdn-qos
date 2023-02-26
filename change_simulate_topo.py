85236668c5274482bde312b858e59cd5fa615410
Hyeong Seon Yoo <sylvesteryoo@naver.com>
2023-02-26 07:27:23 +0000

    changed change_simulate_topo.py to match the new config system

--- change_simulate_topo.py
+++ change_simulate_topo.py
@@ -1,16 +1,25 @@
 from sys import argv
 from yaml import safe_load, dump
+
 def main(argv):
     with open('config/simulate_topo.yml', 'r') as simulate_topo_file:
-       simulate_topo_map = safe_load(simulate_topo_file)
+       simulate_topo_dict = safe_load(simulate_topo_file)
+    with open('config/run_config.yml', 'r') as run_config_file:
+        run_config_dict = safe_load(run_config_file)
+
     change_type = argv[0]
     value = argv[1]
     if (change_type == 'ITZ'):
-        simulate_topo_map['to_test'] = 'Zoo'
-        simulate_topo_map['topology']['Zoo']['details']['filename'] = f'{value}.graphml'
+        simulate_topo_dict['zoo']['details']['filename'] = f'{value}.graphml'
+        run_config_dict['topology'] = 'zoo'
     else:
-        simulate_topo_map['to_test'] = value
+        run_config_dict['topology'] = value
+
+    with open('config/run_config.yml', 'w') as run_config_file:
+        dump(run_config_dict, run_config_file)
+
     with open('config/simulate_topo.yml', 'w') as simulate_topo_file:
-        yee = dump(simulate_topo_map, simulate_topo_file)
+        dump(simulate_topo_dict, simulate_topo_file)
+
 if __name__ == '__main__':
     main(argv[1:])
